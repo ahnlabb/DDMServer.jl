0d1bed3a7bc53b80c844e4869ccd5dadbbb2258b
Johannes Ahnlide <johannes@voxel.se>
2022-05-03 14:14:01 +0200

    feat: ArgParse, port

--- src/DDMServer.jl
+++ src/DDMServer.jl
@@ -4,6 +4,18 @@ using Logging
 using Sockets
 using PyCall
 using TOML
+using ArgParse
+
+function get_args()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--port"
+            help = "specify listening port"
+            arg_type = Int
+            default = 4443
+    end
+    parse_args(s)
+end
 
 function __init__()
     mp(analysis, name, keyfun, keytest) = DDMFramework.multipoint(analysis, name, keyfun; keytest)
@@ -42,6 +54,7 @@ end
 
 
 function julia_main()::Cint
+    args = get_args()
     plugins_dir = "plugins"
 
     if isdir(plugins_dir)
@@ -59,7 +72,7 @@ function julia_main()::Cint
     end
 
     host = ip"127.0.0.1"
-    port = 4443
+    port = args["port"]
     @info "starting server"
     server, task = DDMFramework.serve_ddm_application(;host, port)
     @info "server started"
