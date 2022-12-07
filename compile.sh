73e1c3b7fcd7b9c651de9edfabd4553ce56067fd
OscarAndre1 <bmp13oan@student.lu.se>
2022-12-07 10:15:57 +0100

    Fix: wrong .toml

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+julia --project=. -e 'using Pkg; Pkg.instantiate(); Pkg.add("PackageCompiler")'
 julia --project=. -e 'using PackageCompiler; create_app(".", "compiled"; force=true, include_lazy_artifacts=true)'
